991bfeee93c0d2ea837295f96ac226d7a3e5496a
Yang Wang <30606700+YWang92@users.noreply.github.com>
2023-09-27 10:10:27 +0200

    dot_product.cpp and covering_segments.cpp

--- Data Structures and Algorithms/course1_algorithmic_toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ Data Structures and Algorithms/course1_algorithmic_toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,27 +1,106 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <cassert> 
 
 using std::vector;
 
-long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+
+/**
+ * @brief Computes the maximum dot product between two vectors of integers, representing clicks and prices. 
+ * The function sorts both vectors in descending order and multiplies corresponding elements to calculate the maximum dot product.
+ * 
+ * @param clicks Vector containing the number of clicks.
+ * @param prices Vector containing the price values.
+ * @return The maximum dot product of the two vectors.
+ * 
+ * @pre The size of both vectors should be equal and not exceed 1000. Each element in the vectors should be in the range [0, 100000].
+ */
+
+
+long long max_dot_product(vector<int> clicks, vector<int> prices) {
+  // Assert that both vectors have the same size and meet the preconditions.
+  assert(clicks.size() == prices.size());
+  assert(clicks.size() <= 1000);
+  
+  // Check that each element of both vectors meets the precondition.
+  for (int i = 0; i < clicks.size(); i++) {
+    assert(0 <= clicks[i] && clicks[i] <= 100000);
+    assert(0 <= prices[i] && prices[i] <= 100000);
+  }
+
+  // Sort both vectors in descending order.
+  std::sort(clicks.begin(), clicks.end(), std::greater<int>());
+  std::sort(prices.begin(), prices.end(), std::greater<int>());
+
+  // Calculate the maximum dot product
   long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
+  for (size_t i = 0; i < clicks.size(); i++) {
+    result += ((long long) clicks[i]) * prices[i]; // Cast to long long to avoid overflow
   }
   return result;
 }
 
+
+void run_test_case(vector<int> clicks, vector<int> prices, long long expected) {
+    long long result = max_dot_product(clicks, prices);
+    
+    std::cout << "Test case: clicks = {";
+    for (size_t i = 0; i < clicks.size(); ++i) {
+        std::cout << clicks[i];
+        if (i < clicks.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    
+    std::cout << "}, prices = {";
+    for (size_t i = 0; i < prices.size(); ++i) {
+        std::cout << prices[i];
+        if (i < prices.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    
+    std::cout << "}, expected = " << expected << "\n";
+    std::cout << "Result: " << result << "\n";
+    
+    if (result != expected) {
+        std::cout << "WRONG\n";
+        std::exit(1);
+    } else {
+        std::cout << "OK\n";
+    }
+}
+
+void test_function() {
+    run_test_case({1, 3, 5}, {2, 4, 6}, 44); // 5*6 + 3*4 + 1*2 = 30 + 12 + 2 = 44
+    run_test_case({0, 0, 0}, {1, 1, 1}, 0); // 0*1 + 0*1 + 0*1 = 0
+    run_test_case({100, 2000, 200, 300}, {1, 5, 10, 30}, 64100); // 30*2000 + 10*300 + 5*200 + 1*100 = 60000 + 3000 + 1000 + 100 = 64100
+    // Add more test cases here
+}
+
+
 int main() {
   size_t n;
-  std::cin >> n;
-  vector<int> a(n), b(n);
+  std::cin >> n; // Input the size of vectors
+
+  if (n == -10) {
+    test_function();
+    return 0;
+  }
+  
+  vector<int> a(n), b(n); // Declare vectors a and b of size n
+  
+  // Input the elements of vector a
   for (size_t i = 0; i < n; i++) {
     std::cin >> a[i];
   }
+  
+  // Input the elements of vector b
   for (size_t i = 0; i < n; i++) {
     std::cin >> b[i];
   }
+
+  // Output the maximum dot product of vectors a and b
   std::cout << max_dot_product(a, b) << std::endl;
 }
--- Data Structures and Algorithms/course1_algorithmic_toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
+++ Data Structures and Algorithms/course1_algorithmic_toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
@@ -5,6 +5,14 @@
 
 using std::vector;
 
+
+/**
+ * @brief Finds the minimum number of points such that each segment contains at least one common point.
+ * 
+ * @param segments A reference to a vector containing all the segments.
+ * @return A vector containing the optimal points which are common to the segments.
+ */
+
 struct Segment {
   int start, end;
 };
@@ -12,16 +20,76 @@ struct Segment {
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
   //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+
+  //sort segments by end point
+  std::sort(segments.begin(), segments.end(), [](const Segment& a, const Segment& b) {
+    return a.end < b.end;
+  });
+
+  //initialize current point to the end of the first segment
+  int currentPoint = segments[0].end;
+  points.push_back(currentPoint);
+  for (size_t i = 1; i < segments.size(); ++i) {
+    if (currentPoint < segments[i].start) {
+      currentPoint = segments[i].end;
+      points.push_back(currentPoint);
+    } else {
+      continue;}
   }
   return points;
 }
 
+
+void run_test_case(vector<Segment> segments, vector<int> expected) {
+    vector<int> result = optimal_points(segments);
+    std::cout << "Test case: segments = {";
+    for (size_t i = 0; i < segments.size(); ++i) {
+        std::cout << "{" << segments[i].start << ", " << segments[i].end << "}";
+        if (i < segments.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}, expected points = {";
+    for (size_t i = 0; i < expected.size(); ++i) {
+        std::cout << expected[i];
+        if (i < expected.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}\n";
+    std::cout << "Result: {" ;
+    for (size_t i = 0; i < result.size(); ++i) {
+        std::cout << result[i];
+        if (i < result.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}\n";
+    
+    if (result != expected) {
+        std::cout << "FAIL\n";
+        std::exit(1);
+    } else {
+        std::cout << "PASS\n";
+    }
+}
+
+void test_function() {
+    run_test_case({{1, 3}, {2, 5}, {3, 6}}, {3});
+    run_test_case({{4, 7}, {1, 3}, {2, 5}, {5, 6}}, {3, 6});
+    run_test_case({{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}}, {2, 4, 6});
+}
+
+
 int main() {
   int n;
   std::cin >> n;
+
+  if (n == -10) {
+    test_function();
+    return 0;
+  }
+
   vector<Segment> segments(n);
   for (size_t i = 0; i < segments.size(); ++i) {
     std::cin >> segments[i].start >> segments[i].end;
